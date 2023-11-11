688709b532a3a0e3f47c265bad655408ad86ef9a
gustavodeceserovieira <gustavodeceserovieira@gmail.com>
2023-11-11 12:46:47 -0300

    Implementação de um endpoint que conta a ocorrência de caracteres em  uma frase

--- main.py
+++ main.py
@@ -68,3 +68,32 @@ def bhaskara(bhaskara: calculo):
         return {
             "erro":"Impossível, não há solução real para essa equação"
         }
+
+class Frase(BaseModel):
+    frase: str
+
+
+@app.post("/conta")
+def frase(caracteres: Frase):
+    vogais = 'aeiouAEIOUãêÊúÚáÁàÀÂâ'
+    consoantes = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
+    caracteres_especiais = "!@#$%^&*()-_=+[]{}|;:'\",.<>/?`~"
+    #soma 1 se os caracteres presentes na frase estiverem na lista de vogais
+    contagem_vogais = sum(1 for char in caracteres.frase if char in vogais)
+    #soma 1 se os caracteres presentes na frase estiverem na lista de consoantes
+    contagem_consoantes = sum(1 for char in caracteres.frase if char in consoantes)
+    #soma 1 se os caracteres presentes na frase estiverem na lista de caracteres especiais
+    contagem_caracteres_especiais = sum(1 for char in caracteres.frase if char in caracteres_especiais)
+    #faz a contagem dos espaços na frase
+    espacos = caracteres.frase.count(" ")
+    #faz a contagem de todos os elementos que não são vogais e nem espaços
+    contagem_outros = contagem_consoantes + contagem_caracteres_especiais
+    return{
+        "frase":caracteres.frase,
+        "vogais":contagem_vogais,
+        "espacos":espacos,
+        "outros": contagem_outros
+    }
+
+
+
